5486668edeccde5bdee163d23aa67da558bd44d0
MillanCV <m.castrovilarino@gmail.com>
2025-04-20 09:42:13 +0200

    add socket io

--- project/ws/views.py
+++ project/ws/views.py
@@ -1,10 +1,13 @@
 import json
 
-from fastapi import WebSocket
+import socketio
+from fastapi import FastAPI, WebSocket
+from socketio.asyncio_namespace import AsyncNamespace
 
 from project.ws.router import ws_router
 from project.app import broadcast
 from project.celery_utils import get_task_info
+from project.config import settings
 
 
 @ws_router.websocket("/ws/task_status/{task_id}")
@@ -15,9 +18,7 @@ async def ws_task_status(websocket: WebSocket):
 
     async with broadcast.subscribe(channel=task_id) as subscriber:
         # just in case the task already finish
-        print("1")
         data = get_task_info(task_id)
-        print("2")
         await websocket.send_json(data)
 
         async for event in subscriber:
@@ -34,3 +35,37 @@ async def update_celery_task_status(task_id: str):
         message=json.dumps(get_task_info(task_id)),  # RedisProtocol.publish expect str
     )
     await broadcast.disconnect()
+
+
+class TaskStatusNameSpace(AsyncNamespace):
+
+    async def on_join(self, sid, data):
+        await self.enter_room(sid=sid, room=data["task_id"])
+        # just in case the task already finish
+        await self.emit("status", get_task_info(data["task_id"]), room=data["task_id"])
+
+
+def register_socketio_app(app: FastAPI):
+    mgr = socketio.AsyncRedisManager(settings.WS_MESSAGE_QUEUE)
+    sio = socketio.AsyncServer(
+        async_mode="asgi", client_manager=mgr, logger=True, engineio_logger=True
+    )
+    sio.register_namespace(TaskStatusNameSpace("/task_status"))
+    asgi = socketio.ASGIApp(
+        socketio_server=sio, socketio_path=""  # Try with empty string
+    )
+    app.mount("/ws/socket.io", asgi)
+    print("MOUNTED WS")
+
+
+def update_celery_task_status_socketio(task_id):
+    """
+    This function would be called in Celery worker
+    https://python-socketio.readthedocs.io/en/latest/server.html#emitting-from-external-processes
+    """
+    # connect to the redis queue as an external process
+    external_sio = socketio.RedisManager(settings.WS_MESSAGE_QUEUE, write_only=True)
+    # emit an event
+    external_sio.emit(
+        "status", get_task_info(task_id), room=task_id, namespace="/task_status"
+    )
